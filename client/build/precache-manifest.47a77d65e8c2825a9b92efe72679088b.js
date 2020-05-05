1eb83ebc532cd73746e9e7b4fd7adc10ee80a817
nexterism <nexterism@gmail.com>
2020-05-06 01:22:45 +0500

    app

---
+++ client/build/precache-manifest.47a77d65e8c2825a9b92efe72679088b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977a56e819a522332beae054d35209fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21a048b9e81866d2cd2",
+    "url": "/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "2105f1e3d22ead86dfe7",
+    "url": "/static/css/main.08afdf61.chunk.css"
+  },
+  {
+    "revision": "a21a048b9e81866d2cd2",
+    "url": "/static/js/2.434993da.chunk.js"
+  },
+  {
+    "revision": "2105f1e3d22ead86dfe7",
+    "url": "/static/js/main.ce9dbe8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84b3bfbc056eb6b8642bbf34f583f046.js"
+  "/precache-manifest.47a77d65e8c2825a9b92efe72679088b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
