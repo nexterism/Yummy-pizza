c6d0dff0ec411c4fe76c03973798598268e419de
nexterism <nexterism@gmail.com>
2020-05-06 04:53:43 +0500

    app

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -58,7 +58,7 @@ function UploadProductPage(props) {
     Axios.post("/api/product/uploadProduct", variables).then((response) => {
       if (response.data.success) {
         alert("Product Successfully Uploaded");
-        props.history.push("/");
+        props.history.push("/menu");
       } else {
         alert("Failed to upload Product");
       }
